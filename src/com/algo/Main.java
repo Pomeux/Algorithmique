e6de97144e71e6f555776e6acb04049883b8bb45
Leo <leopldu45@gmail.com>
2025-05-30 22:31:22 +0200

    ajout

--- src/com/algo/Main.java
+++ src/com/algo/Main.java
@@ -1,5 +1,7 @@
 package com.algo;
 
+import java.util.ArrayList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
--- src/com/algo/recherche/Recherche.java
+++ src/com/algo/recherche/Recherche.java
@@ -1,13 +1,13 @@
 
-/*
+
 package com.algo.recherche;
 
-public class Recherche<T> {
+import java.util.ArrayList;
 
+public class Recherche {
 
-
-    public static int recherche_lineaire(T[] tab,T t) {
-        ArrayList<T> s=new ArrayList<T>(2);
+    regarder comment voir si une classe générique implémente une interface
+    public static <T> int  recherche_lineaire(T[] tab,T t) {
 
         int i=0;
         while(i<tab.length) {
@@ -17,17 +17,17 @@ public class Recherche<T> {
         }
         return -1;
     }
-    public static int recherche_lineaire(Array<T>[] tab,T t) {
+    public static <T> int recherche_lineaire(ArrayList<T> tab,T t) {
         int i=0;
-        while(i<tab.length) {
-            if(tab[i].equals(t)) {
+        while(i<tab.size()) {
+            if(tab.get(i).equals(t)) {
                 return i;
             }
         }
         return -1;
     }
 
-    public static int recherche_dichotomique(T[] tab,T t) {
+    public static <T> int recherche_dichotomique(T[] tab,T t) {
 
         int median=tab.length/2;
         int debut=0;
@@ -45,16 +45,16 @@ public class Recherche<T> {
         return -1;
 
     }
-    public static int recherche_dichotomique(Array<T> tab,T t) {
+    public static <T> int recherche_dichotomique(ArrayList<T> tab, T t) {
 
-        int median=tab.length/2;
+        int median=tab.size()/2-1;
         int debut=0;
-        int fin=tab.length-1;
+        int fin=tab.size()-1;
 
         while(debut<fin) {
-            if(tab[median].equals(t)) {
+            if(tab.get(median).equals(t)) {
                 return median;
-            } else if(tab[median]<t) {
+            } else if(tab.get(median)<t) {
                 debut=median+1;
             } else {
                 fin=median-1;
@@ -64,4 +64,3 @@ public class Recherche<T> {
 
     }
 }
-*/
