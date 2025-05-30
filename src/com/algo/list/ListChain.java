99840a5a3619f7e7a0fb83eaea38a120e9ed8ee0
Leo <leopldu45@gmail.com>
2025-05-31 01:52:03 +0200

    ajout

--- src/com/algo/list/ListChain.java
+++ src/com/algo/list/ListChain.java
@@ -1,6 +1,7 @@
 package com.algo.list;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class ListChain<T> implements Iterable<T> {
     private Node<T> debut;
@@ -30,8 +31,39 @@ public class ListChain<T> implements Iterable<T> {
         }
         size+=1;
     }
-    public void delete(T val) {
+    public void deleteDebut() {
 
+    }
+    public void deleteFin() {
+
+        if(debut==null) {
+            throw new NoSuchElementException("a");
+        }
+        if(debut instanceof NodeDouble<T>) {
+
+        } else {
+
+        }
+
+    }
+    public void delete(T val) {
+        Node<T> node=debut;
+        if(debut==null) {
+            throw new NoSuchElementException("a");
+        }
+
+        if(debut.getVal()==val) {
+            gerer ce cas
+        }
+
+        while(!node.getNext().getVal().equals(val)) {
+            node=node.getNext();
+        }
+        if( node.getNext().getNext()==null && node instanceof NodeDouble<T>) {
+            NodeDouble<T> n= (NodeDouble<T>) node.getNext().getNext();
+            n.setPrevious( (NodeDouble<T>) node);
+        }
+        node.setNext(node.getNext().getNext());
     }
     public void delete(int i) {
 
