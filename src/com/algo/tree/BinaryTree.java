eeaf316164c9cff46c812e7d0a2b16ca376c45a3
Leo <leopldu45@gmail.com>
2025-07-04 01:16:26 +0200

    arbre binaire parcours prefix,infix,sufix

--- src/com/algo/tree/BinaryTree.java
+++ src/com/algo/tree/BinaryTree.java
@@ -1,13 +1,18 @@
 package com.algo.tree;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BinaryTree<T> {
     private BinaryTree<T> left;
     private BinaryTree<T> right;
+    private int size=1;
     private T val;
 
     public BinaryTree(T val) {
 
         this.val=val;
+
     }
 
     public BinaryTree(T val,BinaryTree<T> left,BinaryTree<T> right) {
@@ -27,6 +32,9 @@ public class BinaryTree<T> {
     public T getVal() {
         return val;
     }
+    private int get_size() {
+        return size;
+    }
     public T getValRight() {
         if(right==null)
             throw new IllegalArgumentException("à changer");
@@ -44,6 +52,36 @@ public class BinaryTree<T> {
         return left;
     }
 
+    public List<T> post_fix() {
+        return post_fix(this,new ArrayList<T>(size));
+    }
+    public List<T> pre_fix() {
+        return pre_fix(this,new ArrayList<T>(size));
+    }
+    public List<T> infix() {
+        return infix(this,new ArrayList<T>(size));
+    }
+    private List<T> post_fix(BinaryTree<T> tree,List<T> tab) {
+        post_fix(tree.sub_tree_left(),tab);
+        post_fix(tree.sub_tree_right(),tab);
+        tab.addLast(tree.getVal());
+        return tab;
+    }
+    private List<T> pre_fix(BinaryTree<T> tree,List<T> tab) {
+        tab.addLast(tree.getVal());
+        post_fix(tree.sub_tree_left(),tab);
+        post_fix(tree.sub_tree_right(),tab);
+
+        return tab;
+    }
+    private List<T> infix(BinaryTree<T> tree,List<T> tab) {
+        post_fix(tree.sub_tree_left(),tab);
+        tab.addLast(tree.getVal());
+        post_fix(tree.sub_tree_right(),tab);
+
+        return tab;
+    }
+
     @Override
     public boolean equals(Object o) {
         if(this==o) return true;
@@ -53,6 +91,7 @@ public class BinaryTree<T> {
 
         return val.equals(t.val) && left.equals(t.left) && right.equals(t.right);
     }
+
     @Override
     public String toString() {
         return "";
